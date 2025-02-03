8183d4a698199e706ba105520ad317f1fd19bbef
Madhankumar4036 <madhankumar84437@gmail.com>
2025-02-03 12:09:24 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1="hello";
+	    int length = str1.length();
+		System.out.println("print the length : "+length);
+	}
+}
